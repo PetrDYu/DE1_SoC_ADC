8bd3e8fe1f556ab5150ade2fba001017e8dfe3bb
PetrDYu <68391651+PetrDYu@users.noreply.github.com>
2020-09-03 02:02:27 +0300

    Initial commit

---
+++ software/DE1_SoC_ADC/obj/default/main.d
@@ -0,0 +1,11 @@
+obj/default/main.o: main.c ../DE1_SoC_ADC_bsp//HAL/inc/io.h \
+ ../DE1_SoC_ADC_bsp//HAL/inc/alt_types.h ../DE1_SoC_ADC_bsp/system.h \
+ ../DE1_SoC_ADC_bsp/linker.h
+
+../DE1_SoC_ADC_bsp//HAL/inc/io.h:
+
+../DE1_SoC_ADC_bsp//HAL/inc/alt_types.h:
+
+../DE1_SoC_ADC_bsp/system.h:
+
+../DE1_SoC_ADC_bsp/linker.h:
